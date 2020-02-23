c850d8453d61908387f5a46fc6961390c9aa970d
Vincent Servio <vincentservio@gmail.com>
2020-02-23 22:10:34 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount
+    def initialize(discount = nil)
+ @total = 0
+   #@cash_register_with_discount = cash_register_with_discount
+   @discount = discount
+      end
+  
+  def total
+   @total
+   end 
+   
+   
+   def add_item(title, price, quantity = nil)
+     
+     @title = title 
+     @price = price 
+ 
+     #binding.pry
+         if quantity
+            price *= quantity
+            @total += price
+         else 
+             @total += price
+    end 
+   end 
+   
+   def apply_discount 
+        if @discount == nil
+         p "There is no discount to apply."
+       else 
+     result = total * discount / 100
+      @total -= result
+      p "After the discount, the total comes to $800."
+       end
+    end
+    
+    def items 
+    all = []
+    all << @title 
+      
+      #binding.pry
+    end 
+end
